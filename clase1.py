19af0a3433eacc659d1494337d53b19e32c2f446
Arlette Catalan <arlette.catalan@keyinstitute.edu.sv>
2025-05-22 19:05:25 -0600

    Editando clase 1 de Python

--- clase1.py
+++ clase1.py
@@ -4,8 +4,41 @@ Tema:         Variables
 Ejercicio:    Ejercicios con variables
 Descripción:  
 
-Autor:        Arlette Hueleanos
+Autor:        Arlette Bolaños
 Fecha:        2025-05-22
 Estado:       [ Terminado ]
 '''
 
+#Ejercicio 1
+#Pedirle al usuario el total de una cuenta
+#Porcentaje de propina
+#Calcular el total a pagar
+
+total_cuenta = float(input("Ingrese el total de la cuenta: "))
+porcentaje_propina = float(input("Ingrese propina: "))
+propina = total_cuenta * (porcentaje_propina / 100)
+total_a_pagar = total_cuenta + propina
+print("El total de la cuenta es: ", total_cuenta)
+print("La propina es: ", propina)
+print("El total a pagar es: ", total_a_pagar,"con porcentaje de propina de: ", porcentaje_propina, "%")
+
+#Ejercicio 2
+a = int(input())   
+b = int(input())  
+k = int(input())   
+
+
+resultado = str(a // b) + "."
+
+resto = a % b
+if resto < 0:
+    resto = -resto
+b = abs(b)
+
+for i in range(k):
+    resto *= 10
+    digito = resto // b
+    resultado += str(digito)
+    resto = resto % b
+
+print(resultado)
