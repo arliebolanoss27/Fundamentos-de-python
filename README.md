4b69b18dcd0b19ba2870b04416a4a1048f2fab46
Arlette Catalan <arlette.catalan@keyinstitute.edu.sv>
2025-05-22 18:19:18 -0600

    Agregando clase 1 de Python

--- README.md
+++
@@ -1,2 +0,0 @@
-# Fundamentos-de-python
-Repositorio de Arlette Izel Catalán Bolaños para el curso de fundamentos de programación
---
+++ clase1.py
@@ -0,0 +1,11 @@
+'''
+Clase:        Variables, tipos, entradas y salidas
+Tema:         Variables
+Ejercicio:    Ejercicios con variables
+Descripción:  
+
+Autor:        Arlette Hueleanos
+Fecha:        2025-05-22
+Estado:       [ Terminado ]
+'''
+
