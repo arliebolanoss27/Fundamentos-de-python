654f3a1718f9c124a52bc5113ee825f0bbed02d4
Arlette Catalán <arlette.catalan@keyinstitute.edu.sv>
2025-05-30 13:49:13 -0600

    Agregando ejercicio de clase 6 (eliminar los numeros duplicados de una lista)

--- clase3.py
+++ clase3.py
@@ -1,1 +1,10 @@
-#Prueba
+'''
+Clase:        3
+Tema:         Aprendiendo con listas
+Ejercicio:    
+Descripción:  Verificar si una contraseña es segura y calcular el impuesto a pagar por un producto.
+
+Autor:        Arlette Bolaños
+Fecha:        2025-05-22
+Estado:       [ Terminado ]
+'''
