42d67c78f0979102bec902784abd695c3146769a
Arlette Catalán <arlette.catalan@keyinstitute.edu.sv>
2025-06-10 10:34:56 -0600

    correcion de clase 6 subida el 01/06/2025

---
+++ Clase 6/clase6ej2.py
@@ -0,0 +1,15 @@
+#Ejercicio 2
+# Hacer la lista de numeros
+lista3 = list(map(int, input().split(' ')))  # Entrada de lista de números, de la misma forma que antes
+# Encontrar los números líderes
+lideres = []  # Lista para almacenar los números líderes
+for i in range(len(lista3) - 1): # Recorremos la lista hasta el penúltimo elemento
+    # Verificamos si el número en la posición i es líder
+    es_lider = True
+    for j in range(i + 1, len(lista3)): # Recorremos los números a la derecha del número en la posición i
+        if lista3[i] <= lista3[j]: # Si el número en la posición i es menor o igual a alguno de los números a su derecha
+            es_lider = False # Entonces no es líder
+            break
+    if es_lider:
+        lideres.append(lista3[i])# # Si es líder, lo agregamos a la lista de líderes
+print(' '.join(map(str, lideres)))# Y por último, ya imprimimos los números líderes por separado jeje.
